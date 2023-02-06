62097532950b1b5dfb80bd170d5fa074016a2640
purinutss <purinuts2541@gmail.coms>
2023-02-06 10:21:31 +0700

    config essential middlewares

--- src/app.js
+++ src/app.js
@@ -1,2 +1,31 @@
 // const { sequelize } = require("./models");
 // sequelize.sync({ force: true });
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+app.use(morgan("dev")); //log
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: { message: "many requested, please try again" },
+  })
+); //limit request
+app.use(helmet()); //protect hijack
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server run on port ${port}`));
